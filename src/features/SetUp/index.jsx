10c3d918baf6afe5c1b6f8f86b157d55dcc7e109
snovosel <novosel991@gmail.com>
2020-02-03 18:48:00 -0500

    correct link

--- src/features/SetUp/index.jsx
+++ src/features/SetUp/index.jsx
@@ -37,7 +37,7 @@ class SetUp extends Component {
   }
 
   handleCopyClick(e) {
-    window.navigator.clipboard.writeText(`psst.novowd.com/${this.state.uri}`);
+    window.navigator.clipboard.writeText(`demo.novowd.com/${this.state.uri}`);
 
     this.setState({
       copied: true
